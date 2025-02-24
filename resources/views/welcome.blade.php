aef0c657f07ac5dd785c4efa40411ecc7aba6f5c
ckckckcz <rriovld@gmail.com>
2025-02-24 09:58:40 +0700

    Update welcome page content and change background color in app.css

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,8 +2,8 @@
 @section('content')
 <section class="">
     <div class="py-8 px-4 mx-auto max-w-screen-xl text-left lg:py-16">
-        <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">We invest in the world’s potential</h1>
-        <p class="mb-8 text-lg font-normal text-gray-300  lg:text-xl">Here at Flowbite we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</p>
+        <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">Rasakan Euforia Konser Tak Terlupakan</h1>
+        <p class="mb-4 text-4xl font-bold tracking-tight leading-none text-white md:text-5xl lg:text-2xl">Pesan tiket konser favoritmu dengan mudah dan cepat. Jadilah bagian dari momen spektakuler yang penuh semangat dan kenangan indah!</p>
     </div>
 </section>
 @endsection
