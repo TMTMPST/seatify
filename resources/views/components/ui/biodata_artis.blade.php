08b1ef4ebb524fc3317b5dea0adedd62a4254d9c
Vidi Joshubzky Saviola <103508462+TMTMPST@users.noreply.github.com>
2025-02-24 20:12:30 +0700

    just fixing detail concert

--- resources/views/components/ui/biodata_artis.blade.php
+++ resources/views/components/ui/biodata_artis.blade.php
@@ -1,9 +1,7 @@
-@extends('layouts.component')
-@section('component')
+
 <section class="bg-white rounded-t-xl">
     <div class="py-8 px-4 mx-auto max-w-screen-xl text-left lg:py-16 space-y-5">
         hoax jir
     </div>
 </section>
 
-@endsection
--- resources/views/components/ui/tiket_konser.blade.php
+++ resources/views/components/ui/tiket_konser.blade.php
@@ -1,5 +1,3 @@
-@extends('layouts.component')
-@section('component')
 <section class="bg-white rounded-t-xl">
     <div class="py-8 px-4 mx-auto max-w-screen-xl text-left lg:py-16 space-y-5">
         <div class="flex flex-col justify-between">
@@ -129,4 +127,4 @@
     </div>
 </section>
 
-@endsection
+
