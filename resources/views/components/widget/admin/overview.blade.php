9f5ccadd81d180e693023a40a2167c22fde927e6
ckckckcz <rriovld@gmail.com>
2025-02-25 08:18:05 +0700

    Add admin overview widget to admin dashboard

---
+++ resources/views/components/widget/admin/overview.blade.php
@@ -0,0 +1,20 @@
+<div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5">
+    <div>
+        <div class="block hover:border-blue-900 hover:border w-full p-6 bg-white border border-gray-200 rounded-xl shadow-xs">
+            <h5 class="mb-1 text-md font-normal tracking-tight text-gray-400">Jumlah Konser</h5>
+            <p class="text-2xl font-bold text-gray-900">10 Konser Tersedia</p>
+        </div>
+    </div>
+    <div>
+        <div class="block hover:border-blue-900 hover:border w-full p-6 bg-white border border-gray-200 rounded-xl shadow-xs">
+            <h5 class="mb-1 text-md font-normal tracking-tight text-gray-400">Jumlah Pengguna</h5>
+            <p class="text-2xl font-bold text-gray-900">1.500 Pengguna</p>
+        </div>
+    </div>
+    <div>
+        <div class="block hover:border-blue-900 hover:border w-full p-6 bg-white border border-gray-200 rounded-xl shadow-xs">
+            <h5 class="mb-1 text-md font-normal tracking-tight text-gray-400">Total Pendapatan</h5>
+            <p class="text-2xl font-bold text-gray-900">Rp 50.000.000</p>
+        </div>
+    </div>
+</div>
