3f93f15f797c3ac1970daa9cfed54495e4b21041
ckckckcz <rriovld@gmail.com>
2025-02-26 17:02:46 +0700

    Refactor navbar component to improve layout and update menu items for better navigation

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,31 +1,30 @@
 <nav>
-  <div class="max-w-screen-xl mx-auto flex flex-wrap items-center justify-between p-6">
-      <a href="https://flowbite.com/" class="flex items-center space-x-3 rtl:space-x-reverse">
-          <img src="https://flowbite.com/docs/images/logo.svg" class="h-8" alt="Flowbite Logo" />
-          <span class="text-2xl font-semibold text-white">Seatify</span>
-      </a>
+    <div class="max-w-screen-xl mx-auto flex flex-wrap items-center justify-between p-6">
+        <a href="https://flowbite.com/" class="flex items-center space-x-3 rtl:space-x-reverse">
+            <img src="https://flowbite.com/docs/images/logo.svg" class="h-8" alt="Flowbite Logo" />
+            <span class="text-2xl font-semibold text-white">Seatify</span>
+        </a>
 
-      <div class="flex items-center md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-          <a href="/login" class="hidden lg:block">
-              <button type="button" class="bg-gray-100 hover:bg-gray-300 text-gray-900 font-medium rounded-lg text-sm px-4 py-2 focus:ring-4 focus:outline-none focus:ring-blue-300">
-                  Masuk
-              </button>
-          </a>
-          <button data-collapse-toggle="navbar-cta" type="button" class="md:hidden p-2 w-10 h-10 text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:ring-2 focus:ring-gray-200" aria-controls="navbar-cta" aria-expanded="false">
-              <span class="sr-only">Open main menu</span>
-              <svg class="w-5 h-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14" aria-hidden="true">
-                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
-              </svg>
-          </button>
-      </div>
+        <div class="flex items-center md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+            <a href="/login" class="hidden lg:block">
+                <button type="button" class="bg-gray-100 hover:bg-gray-300 text-gray-900 font-medium rounded-lg text-sm px-4 py-2 focus:ring-4 focus:outline-none focus:ring-blue-300">
+                    Masuk
+                </button>
+            </a>
+            <button data-collapse-toggle="navbar-cta" type="button" class="md:hidden p-2 w-10 h-10 text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:ring-2 focus:ring-gray-200" aria-controls="navbar-cta" aria-expanded="false">
+                <span class="sr-only">Open main menu</span>
+                <svg class="w-5 h-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14" aria-hidden="true">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
+                </svg>
+            </button>
+        </div>
 
-      <div id="navbar-cta" class="hidden md:flex md:w-auto md:order-1">
-          <ul class="flex flex-col md:flex-row font-medium p-4 md:p-0 mt-4 md:mt-0 md:space-x-8 rtl:space-x-reverse">
-              <li><a href="/" class="block py-2 px-5 text-white">Home</a></li>
-              <li><a href="#" class="block py-2 px-5 text-white">About</a></li>
-              <li><a href="#" class="block py-2 px-5 text-white">Services</a></li>
-              <li><a href="#" class="block py-2 px-5 text-white">Contact</a></li>
-          </ul>
-      </div>
-  </div>
+        <div id="navbar-cta" class="hidden md:flex md:w-auto md:order-1">
+            <ul class="flex flex-col md:flex-row font-medium p-4 md:p-0 mt-4 md:mt-0 md:space-x-8 rtl:space-x-reverse">
+                <li><a href="/" class="block py-2 px-5 text-white">Home</a></li>
+                <li><a href="#" class="block py-2 px-5 text-white">Konser</a></li>
+                <li><a href="#" class="block py-2 px-5 text-white">Kontak</a></li>
+            </ul>
+        </div>
+    </div>
 </nav>
