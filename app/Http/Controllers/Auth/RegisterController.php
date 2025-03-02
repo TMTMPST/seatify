40811327ae33a607c7d7453d68d046bbef244448
ckckckcz <rriovld@gmail.com>
2025-03-02 18:52:21 +0700

    Improve registration process by adding validation and enhancing form layout

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -8,20 +8,37 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 
-class RegisterController extends Controller {
-    public function register(Request $request) {
-        $request->validate([
+class RegisterController extends Controller
+{
+    private const USER_DEFAULT_LEVEL = 1;
+
+    public function register(Request $request)
+    {
+        $validator = $this->validateRequest($request);
+
+        if ($validator->fails()) {
+            return back()->withErrors($validator)->withInput();
+        }
+
+        $this->createUser($request);
+
+        return back()->with('success', 'Registrasi berhasil!');
+    }
+
+    private function validateRequest(Request $request)
+    {
+        return Validator::make($request->all(), [
             'email' => 'required|email|unique:users,email',
             'password' => 'required|min:8',
         ]);
+    }
 
+    private function createUser(Request $request): void
+    {
         User::create([
             'email' => $request->email,
             'password' => Hash::make($request->password),
-            'level' => 1,
+            'level' => self::USER_DEFAULT_LEVEL,
         ]);
-
-        return redirect()->back()->with('success', 'Registrasi berhasil!');
     }
 }
-
