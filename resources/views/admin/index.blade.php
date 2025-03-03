4901a97e218ff193327cc12162e603bdecfc6299
ckckckcz <rriovld@gmail.com>
2025-03-03 09:53:58 +0700

    Add concert list component to admin dashboard

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -2,4 +2,5 @@
 @section('admin')
     @include('components.widget.admin.overview')
     <hr class="h-px my-8 bg-gray-200 border-0">
+    @include("components.widget.admin.data_konser")
 @endsection
