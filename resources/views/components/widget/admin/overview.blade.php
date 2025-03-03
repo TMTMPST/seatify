d528838d7a4269aaa078b2bac98ecac64a593d84
ckckckcz <rriovld@gmail.com>
2025-03-03 09:36:01 +0700

    Enhance admin overview widget with improved statistics display and visual consistency

--- resources/views/components/widget/admin/overview.blade.php
+++ resources/views/components/widget/admin/overview.blade.php
@@ -1,20 +1,23 @@
 <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5">
     <div>
         <div class="block hover:border-blue-900 hover:border w-full p-6 bg-white border border-gray-200 rounded-xl shadow-xs">
-            <h5 class="mb-1 text-md font-normal tracking-tight text-gray-400">Jumlah Konser</h5>
-            <p class="text-2xl font-bold text-gray-900">10 Konser Tersedia</p>
+            <h5 class="mb-1 text-md font-medium tracking-tight text-gray-400">Jumlah Konser</h5>
+            <p class="mb-2 text-4xl font-bold text-gray-900">10</p>
+            <h5 class="text-md font-medium tracking-tight text-green-800">5% dari bulan lalu</h5>
         </div>
     </div>
     <div>
         <div class="block hover:border-blue-900 hover:border w-full p-6 bg-white border border-gray-200 rounded-xl shadow-xs">
-            <h5 class="mb-1 text-md font-normal tracking-tight text-gray-400">Jumlah Pengguna</h5>
-            <p class="text-2xl font-bold text-gray-900">1.500 Pengguna</p>
+            <h5 class="mb-1 text-md font-medium tracking-tight text-gray-400">Jumlah Pengguna</h5>
+            <p class="mb-2 text-4xl font-bold text-gray-900">1.500</p>
+            <h5 class="text-md font-medium tracking-tight text-green-800">5% dari bulan lalu</h5>
         </div>
     </div>
     <div>
         <div class="block hover:border-blue-900 hover:border w-full p-6 bg-white border border-gray-200 rounded-xl shadow-xs">
-            <h5 class="mb-1 text-md font-normal tracking-tight text-gray-400">Total Pendapatan</h5>
-            <p class="text-2xl font-bold text-gray-900">Rp 50.000.000</p>
+            <h5 class="mb-1 text-md font-medium tracking-tight text-gray-400">Total Pendapatan</h5>
+            <p class="mb-2 text-4xl font-bold text-gray-900">Rp 50.000.000</p>
+            <h5 class="text-md font-medium tracking-tight text-green-800">5% dari bulan lalu</h5>
         </div>
     </div>
 </div>
