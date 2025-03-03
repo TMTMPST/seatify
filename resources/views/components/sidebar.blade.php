64e39de5507c8bc62be584489b82b4106652866b
ckckckcz <rriovld@gmail.com>
2025-03-03 09:50:55 +0700

    Update sidebar component with new icons, labels, and logout functionality

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -4,9 +4,7 @@
             @foreach ($menuItems as $item)
                 <li>
                     <a href="{{ $item['url'] }}" class="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group">
-                        <svg class="w-5 h-5 text-gray-500 transition duration-75 group-hover:text-gray-900" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="{{ $item['iconViewBox'] }}">
-                            {!! $item['iconPath'] !!}
-                        </svg>
+                        {!! $item['iconPath'] !!}
                         <span class="ms-3">{{ $item['label'] }}</span>
                         @if (isset($item['badge']))
                             <span class="inline-flex items-center justify-center px-2 ms-3 text-sm font-medium text-gray-800 bg-gray-100 rounded-full">{{ $item['badge'] }}</span>
@@ -14,6 +12,15 @@
                     </a>
                 </li>
             @endforeach
+            <hr class="h-px bg-gray-200 border-0">
+            <li>
+                <form action="{{ route('logout') }}" method="POST" class="cursor-pointer flex items-center p-2 text-red-700 rounded-lg hover:bg-gray-100 group">
+                    @csrf
+                    <button class="cursor-pointer">
+                        Keluar
+                    </button>
+                </form>
+            </li>
         </ul>
     </div>
 </aside>
