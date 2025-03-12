dc7a2415e26fa1017c6684df8f958f6856d57ab5
ckckckcz <rriovld@gmail.com>
2025-03-13 05:22:07 +0700

    Enhance Tailwind CSS configuration by adding custom color palette; improve theme customization

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,21 @@
 module.exports = {
-    content: [
+  content: [
       "./resources/**/*.blade.php",
       "./resources/**/*.js",
       "./resources/**/*.vue",
       "./node_modules/flowbite/**/*.js"
-    ],
-    theme: {
-      extend: {},
-    },
-    plugins: [
-        require('flowbite/plugin')
-    ],
-  }
+  ],
+  theme: {
+      extend: {
+          colors: {
+              primary: "#1E293B",
+              secondary: "#FACC15",
+              textPrimary: "#FFFFFF",
+              textSecondary: "#1E293B",
+          }
+      },
+  },
+  plugins: [
+      require('flowbite/plugin')
+  ],
+};
